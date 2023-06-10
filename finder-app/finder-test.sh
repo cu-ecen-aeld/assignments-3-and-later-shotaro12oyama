c51df60c7f5767f4ae11f0a3d7d6df2475b70791
Ubuntu <ubuntu@ip-172-31-19-25.ec2.internal>
2023-06-10 14:02:15 +0000

    update for finder-test.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -58,7 +58,7 @@ do
 	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR" > /tmp/assignment4-result.txt )
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR" > /tmp/assignment4-result.txt )
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
