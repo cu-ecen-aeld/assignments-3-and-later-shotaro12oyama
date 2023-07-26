fdce9e42f24a0580fd10eceb3841838c62907719
Ubuntu <ubuntu@ip-172-31-19-25.ec2.internal>
2023-07-26 07:20:05 +0000

    update

--- aesd-char-driver/aesdchar.h
+++ aesd-char-driver/aesdchar.h
@@ -5,22 +5,21 @@
  *      Author: Dan Walkes
  */
 
-ifndef AESD_CHAR_DRIVER_AESDCHAR_H_
-define AESD_CHAR_DRIVER_AESDCHAR_H_
+#ifndef AESD_CHAR_DRIVER_AESDCHAR_H_
+#define AESD_CHAR_DRIVER_AESDCHAR_H_
 
-define AESD_DEBUG 1  //Remove comment on this line to enable debug
+#define AESD_DEBUG 1  //Remove comment on this line to enable debug
 
-undef PDEBUG             /* undef it, just in case */
-ifdef AESD_DEBUG
-#  ifdef __KERNEL__
-     /* This one if debugging is on, and kernel space */
-#    define PDEBUG(fmt, args...) printk( KERN_DEBUG "aesdchar: " fmt, ## args)
-#  else
-     /* This one for user space */
-#    define PDEBUG(fmt, args...) fprintf(stderr, fmt, ## args)
-#  endif
+#undef PDEBUG             /* undef it, just in case */
+#ifdef AESD_DEBUG
+
+#ifdef __KERNEL__ /* This one if debugging is on, and kernel space */
+#define PDEBUG(fmt, args...) printk( KERN_DEBUG "aesdchar: " fmt, ## args)
+#else /* This one for user space */
+#define PDEBUG(fmt, args...) fprintf(stderr, fmt, ## args)
+#endif
 #else
-#  define PDEBUG(fmt, args...) /* not debugging: nothing */
+#define PDEBUG(fmt, args...) /* not debugging: nothing */
 #endif
 
 struct aesd_dev
